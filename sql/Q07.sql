81ce89b04e63f8e895913beea56c2584e62c64d5
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-04 08:31:52 +0300

    Update Q07.sql
    Νομίζω έβαλα το τέτοιο για να τυπώνει το ελάχιστο, αλλά αν δύο έχουν ίδιο μέσο όρο, τυπώνει μόνο το ένα.

--- sql/Q07.sql
+++ sql/Q07.sql
@@ -1,3 +1,5 @@
-SELECT ID, AVG(experience_level) AS tmp FROM 
+SELECT ID, AVG(experience_level) AS average_experience_level FROM 
 (SELECT DISTINCT festival.festival_id AS ID, staff.staff_id AS yolo, staff.experience_level FROM festival natural JOIN music_event NATURAL JOIN performance natural join staff) AS GOTOHELL
-GROUP BY ID;
+GROUP BY ID
+ORDER BY average_experience_level ASC
+LIMIT 1;
