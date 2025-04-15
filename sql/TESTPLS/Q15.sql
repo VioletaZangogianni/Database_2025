937d004779e2d2ac00d3820f6d55a32d66d11dbf
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-15 16:28:50 +0300

    Create Q15.sql
    ίσως θέλει διαφορετικό στο σκορ, αλλά νομίζω αυτό είναι

---
+++ sql/TESTPLS/Q15.sql
@@ -0,0 +1,7 @@
+WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
+A (artist_id, performance_id) AS (SELECT artist_id, performance_id FROM artist INNER JOIN performer NATURAL JOIN performance ON artist.artist_performer_id = performer.performer_id),
+B (artist_id, performance_id) AS (SELECT artist_id, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
+art_perf (artist_id, performance_id) AS (SELECT * FROM A UNION SELECT * FROM B)
+SELECT visitor_id, artist_id, interpretation + overall_impression AS score FROM visitor NATURAL JOIN review NATURAL JOIN art_perf
+ORDER BY score DESC
+LIMIT 5;
