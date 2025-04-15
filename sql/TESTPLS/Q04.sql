031b1229c0e3759ce8449099953c1b3c79b4b5dc
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-15 13:22:38 +0300

    Update Q04.sql
    ουπς, ξέχασα να συνδέσω review με καλλιτέχνη

--- sql/TESTPLS/Q04.sql
+++ sql/TESTPLS/Q04.sql
@@ -1,2 +1,6 @@
-SELECT AVG(interpretation) AS avg_int, AVG(overall_impression) AS avg_ov_imp FROM review
+WITH band_rev (band_id, interpretation, overall_impression) AS (SELECT band_id, interpretation, overall_impression FROM band INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON band.band_performer_id = performer_id),
+A (artist_id, interpretation, overall_impression) AS (SELECT artist_id, interpretation, overall_impression FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON artist.artist_performer_id = performer.performer_id),
+B (artist_id, interpretation, overall_impression) AS (SELECT artist_it, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev),
+art_rev AS (SELECT * FROM  UNION SELECT * FROM B)
+SELECT AVG(interpretation) AS avg_int, AVG(overall_impression) AS avg_ov_imp FROM art_rev
 WHERE artist_id = 1;
