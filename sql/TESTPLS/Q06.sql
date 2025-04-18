c2751e30c00a48785f55a3be8f87e2b46f489cc4
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-18 10:31:32 +0300

    Update Q06.sql
    άλλαξα για να τρέχει με 2.1

--- sql/TESTPLS/Q06.sql
+++ sql/TESTPLS/Q06.sql
@@ -1,5 +1,5 @@
-SELECT visitor_id, music_event_id, AVG((interpretation + sound_and_lighting + stage_presence + review_organization + overall_impression)/5) AS avg_rev
+SELECT visitor_id, music_event_id, AVG((review_interpretation + review_sound_and_lighting + review_stage_presence + review_organization + review_overall_impression)/5) AS avg_rev
 FROM visitor NATURAL JOIN review
 NATURAL JOIN performance NATURAL JOIN music_event
 GROUP BY visitor_id, music_event_id
-WHERE visitor_id = 5;
+HAVING visitor_id = 5;
