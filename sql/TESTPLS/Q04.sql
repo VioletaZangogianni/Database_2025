d631ccd42a27035a8d77c296281e91a78d9b42aa
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-18 10:30:08 +0300

    Update Q04.sql
    διόρθωσα τυπογραφικά που είχα κάνει
    άλλαξα για να τρέχει με το 2.1

--- sql/TESTPLS/Q04.sql
+++ sql/TESTPLS/Q04.sql
@@ -1,6 +1,6 @@
-WITH band_rev (band_id, interpretation, overall_impression) AS (SELECT band_id, interpretation, overall_impression FROM band INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON band.band_performer_id = performer_id),
-A (artist_id, interpretation, overall_impression) AS (SELECT artist_id, interpretation, overall_impression FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON artist.artist_performer_id = performer.performer_id),
-B (artist_id, interpretation, overall_impression) AS (SELECT artist_it, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev),
-art_rev AS (SELECT * FROM  UNION SELECT * FROM B)
+WITH band_rev (band_id, interpretation, overall_impression) AS (SELECT band_id, review_interpretation, review_overall_impression FROM band INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON band.band_performer_id = performer_id),
+A (artist_id, interpretation, overall_impression) AS (SELECT artist_id, review_interpretation, review_overall_impression FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON artist.artist_performer_id = performer.performer_id),
+B (artist_id, interpretation, overall_impression) AS (SELECT artist_id, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev),
+art_rev AS (SELECT * FROM A UNION SELECT * FROM B)
 SELECT AVG(interpretation) AS avg_int, AVG(overall_impression) AS avg_ov_imp FROM art_rev
 WHERE artist_id = 1;
