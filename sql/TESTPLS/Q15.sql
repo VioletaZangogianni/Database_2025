4795f713ef6170c4e0af7563e201955b3bc62656
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-18 11:28:21 +0300

    Update Q15.sql
    λειτουργεί με 2.1

--- sql/TESTPLS/Q15.sql
+++ sql/TESTPLS/Q15.sql
@@ -2,6 +2,6 @@ WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM
 A (artist_id, performance_id) AS (SELECT artist_id, performance_id FROM artist INNER JOIN performer NATURAL JOIN performance ON artist.artist_performer_id = performer.performer_id),
 B (artist_id, performance_id) AS (SELECT artist_id, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
 art_perf (artist_id, performance_id) AS (SELECT * FROM A UNION SELECT * FROM B)
-SELECT visitor_id, artist_id, interpretation + overall_impression AS score FROM visitor NATURAL JOIN review NATURAL JOIN art_perf
+SELECT visitor_id, artist_id, review_interpretation + review_overall_impression AS score FROM visitor NATURAL JOIN review NATURAL JOIN art_perf
 ORDER BY score DESC
 LIMIT 5;
