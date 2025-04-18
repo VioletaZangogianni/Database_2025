c30c9c1d24fb2d8ecae57600e95cc3f6ad1952b1
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-18 11:26:49 +0300

    Update Q14.sql
    διάφορες διορθώσεις, δουλεύει με 2.1

--- sql/TESTPLS/Q14.sql
+++ sql/TESTPLS/Q14.sql
@@ -2,9 +2,8 @@ WITH band_perf_year (band_id, performance_id, fest_year) AS (SELECT band_id, per
 A (genre_id, performance_id, fest_year) AS (SELECT genre_id, performance_id, festival_fest_year FROM genre NATURAL JOIN art2genre NATURAL JOIN artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN music_event NATURAL JOIN festival ON artist.artist_performer_id = performer.performer_id),
 B (genre_id, performance_id, fest_year) AS (SELECT genre_id, performance_id, fest_year FROM genre NATURAL JOIN art2genre NATURAL JOIN artist NATURAL JOIN members NATURAL JOIN band_perf_year),
 g_perf_year (genre_id, performance_id, fest_year) AS (SELECT * FROM A UNION SELECT * FROM B),
-g_year_apps (genre_id, fest_year, apps) AS (SELECT genre_id, fest_year, COUNT(*) FROM g_perf_year GROUP BY genre_id, fest_year),
-g_two_years (genre_id, year1) AS (SELECT Y1.genre_id, Y1.fest_year FROM g_year_apps Y1, g_year_apps Y2 WHERE Y1.fest_year + 1 = Y2.fest_year AND Y1.apps = Y2.apps)
+g_year_apps (genre_id, fest_year, apps) AS (SELECT genre_id, fest_year, COUNT(*) FROM g_perf_year GROUP BY genre_id, fest_year)
 
-SELECT * FROM g__year_apps
+SELECT * FROM g_year_apps WHERE (genre_id, fest_year) IN (SELECT Y1.genre_id, Y1.fest_year FROM g_year_apps Y1, g_year_apps Y2 WHERE Y1.fest_year + 1 = Y2.fest_year AND Y1.apps = Y2.apps)
 GROUP BY genre_id, fest_year
-HAVING apps >= 3 AND genre_id, fest_year IN g_two_years;
+HAVING apps >= 3;
