a26f76d4a93314fea2426a6e04f32eea61a0cdaa
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-22 17:16:57 +0300

    Update Q04.sql
    Τίποτα σημαντικό, άλλαξα τα ονόματα των στηλών, και φαίνεται και το όνομα του καλλιτέχνη.

--- sql/TESTPLS/Q04.sql
+++ sql/TESTPLS/Q04.sql
@@ -1,6 +1,7 @@
 WITH band_rev (band_id, interpretation, overall_impression) AS (SELECT band_id, review_interpretation, review_overall_impression FROM band INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON band.band_performer_id = performer_id),
-A (artist_id, interpretation, overall_impression) AS (SELECT artist_id, review_interpretation, review_overall_impression FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON artist.artist_performer_id = performer.performer_id),
-B (artist_id, interpretation, overall_impression) AS (SELECT artist_id, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev),
+A (artist_id, artist_name, interpretation, overall_impression) AS (SELECT artist_id, artist_name, review_interpretation, review_overall_impression FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON artist.artist_performer_id = performer.performer_id),
+B (artist_id, artist_name, interpretation, overall_impression) AS (SELECT artist_id, artist_name, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev),
 art_rev AS (SELECT * FROM A UNION SELECT * FROM B)
-SELECT AVG(interpretation) AS avg_int, AVG(overall_impression) AS avg_ov_imp FROM art_rev
-WHERE artist_id = 1;
+SELECT artist_name AS 'Name', AVG(interpretation) AS 'Average Interpretation', AVG(overall_impression) AS 'Average Overall Impression' FROM art_rev
+WHERE artist_id = 1
+GROUP BY artist_name;
