74c685b8f7ff0b8e44fd307d0b3853b7b6aeae79
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-22 17:55:51 +0300

    Update Q02.sql

--- sql/TESTPLS/Q02.sql
+++ sql/TESTPLS/Q02.sql
@@ -3,7 +3,7 @@ WITH band_perf (band_id, performance_id, music_event_id) AS (SELECT band_id, per
 	 B (artist_id, artist_name, genre_desc, fest_year) AS (SELECT artist_id, artist_name, genre_desc, festival_fest_year FROM art2genre NATURAL JOIN artist NATURAL JOIN members NATURAL JOIN band_perf NATURAL JOIN music_event NATURAL JOIN festival),
      art_g_year (artist_id, artist_name, genre_desc, fest_year) AS (SELECT * FROM A UNION SELECT * FROM B),
      g_artists AS (SELECT artist_id, genre_desc FROM art_g_year WHERE genre_desc = 'Rock')
-SELECT DISTINCT artist_id AS id, artist_name, case
+SELECT DISTINCT artist_id AS id, artist_name AS 'Name', case
 	WHEN 2018 IN (SELECT fest_year FROM (SELECT artist_id, fest_year FROM art_g_year WHERE artist_id = id) AS T) THEN 'YES'
     ELSE 'NO'
 END AS wasThisYear FROM art_g_year
