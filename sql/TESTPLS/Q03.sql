21442a5b06a60d321502959f396049c3ba33a23a
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-22 17:13:03 +0300

    Update Q03.sql
    δουλεύει με 2.2.1
    άλλαξα ώστε να τυπώνει έτος και όνομα φεστιβάλ και όνομα καλλιτέχνη, αντί για τα ids

--- sql/TESTPLS/Q03.sql
+++ sql/TESTPLS/Q03.sql
@@ -1,7 +1,7 @@
-WITH band_perf (band_id, performance_id, performance_type) AS (SELECT band_id, performance_id, performance_type FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
-	 A (artist_id, festival_id, performance_type) AS (SELECT artist_id, festival_id, performance_type FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN music_event ON artist.artist_performer_id = performer.performer_id),
-	 B (artist_id, festival_id, performance_type) AS (SELECT artist_id, festival_id, performance_type FROM artist NATURAL JOIN members NATURAL JOIN band_perf NATURAL JOIN music_event),
-     art_perf (festival_id, artist_id) AS (SELECT festival_id, artist_id, performance_type FROM A UNION SELECT festival_id, artist_id FROM B WHERE performance_type = 'Warm-up')
-SELECT festival_id, artist_id FROM art_perf
-	GROUP BY festival_id, artist_id
+WITH band_perf (band_id, performance_id, performance_type) AS (SELECT band_id, performance_id, type_performance_desc FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
+	 A (artist_id, artist_name, fest_year, fest_name, performance_type) AS (SELECT artist_id, artist_name, festival_fest_year, festival_name, type_performance_desc FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN music_event NATURAL JOIN festival ON artist.artist_performer_id = performer.performer_id),
+	 B (artist_id, artist_name, fest_year, fest_name, performance_type) AS (SELECT artist_id, artist_name, festival_fest_year, festival_name, performance_type FROM artist NATURAL JOIN members NATURAL JOIN band_perf NATURAL JOIN music_event NATURAL JOIN festival),
+     art_perf (fest_year, fest_name, artist_id, artist_name) AS (SELECT fest_year, fest_name, artist_id, artist_name FROM A UNION SELECT fest_year, fest_name, artist_id, artist_name FROM B WHERE performance_type = 'Warm-up')
+SELECT fest_year, fest_name, artist_name FROM art_perf
+	GROUP BY fest_year, fest_name, artist_name
 	HAVING COUNT(artist_id) > 2;
