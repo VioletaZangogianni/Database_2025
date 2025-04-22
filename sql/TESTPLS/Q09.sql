03ff17de8ef59a803e84a0b7f0d494d84db93180
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-22 23:41:22 +0300

    Update Q09.sql

--- sql/TESTPLS/Q09.sql
+++ sql/TESTPLS/Q09.sql
@@ -1,4 +1,4 @@
-WITH vis_event (visitor_id, shows) AS (SELECT visitor_id, COUNT(DISTINCT music_event_id) FROM visitor NATURAL JOIN ticket NATURAL JOIN music_event WHERE music_event_date BETWEEN '2025-3-3' AND DATE_ADD(music_event.music_event_date, INTERVAL 1 YEAR) GROUP BY visitor_id)
-SELECT V.shows, V.visitor_id FROM vis_event V
+WITH vis_event (visitor_id, visitor_name, shows) AS (SELECT visitor_id, CONCAT(visitor_name, ' ', visitor_surname) AS full_name, COUNT(DISTINCT music_event_id) FROM visitor NATURAL JOIN ticket NATURAL JOIN music_event WHERE music_event_date BETWEEN '2020-3-3' AND DATE_ADD(music_event.music_event_date, INTERVAL 1 YEAR) GROUP BY visitor_id, full_name)
+SELECT V.shows AS Shows, V.visitor_id AS ID, V.visitor_name AS 'Name' FROM vis_event V
 WHERE V.shows IN (SELECT shows FROM vis_event WHERE visitor_id <> V.visitor_id AND shows >= 3)
 ORDER BY V.shows ASC;
