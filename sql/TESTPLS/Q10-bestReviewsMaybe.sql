cbe4988af1005f2fee0610ca627fc9fff14a67aa
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-04-26 11:07:41 +0300

    Update Q10-bestReviewsMaybe.sql
    έβαλα έλεγχο ότι τα 2 genres είναι διαφορετικά

--- sql/TESTPLS/Q10-bestReviewsMaybe.sql
+++ sql/TESTPLS/Q10-bestReviewsMaybe.sql
@@ -4,7 +4,7 @@ A (genre_desc, artist_id, rev) AS (SELECT genre_desc, artist_id, review_overall_
 B (genre_desc, artist_id, rev) AS (SELECT genre_desc, artist_id, review_overall_impression + review_interpretation FROM art2genre NATURAL JOIN artist NATURAL JOIN members NATURAL JOIN band_perf NATURAL JOIN review),
 g_art_perf (genre_desc, artist_id, rev) AS (SELECT * FROM A UNION SELECT * FROM B)
 
-SELECT G1.genre_desc AS first_genre, G2.genre_desc AS second_genre, AVG(G1.rev) AS score FROM g_art_perf G1, g_art_perf G2 WHERE G1.artist_id = G2.artist_id
+SELECT G1.genre_desc AS first_genre, G2.genre_desc AS second_genre, AVG(G1.rev) AS score FROM g_art_perf G1, g_art_perf G2 WHERE G1.artist_id = G2.artist_id AND G1.genre_desc < G2.genre_desc
 GROUP BY first_genre, second_genre
 ORDER BY score DESC
 LIMIT 3;
