a8aa86793065b2e540ec51c6199d41b5174aa8c4
VioletaZangogianni <el22213@mail.ntua.gr>
2025-04-29 11:53:58 +0300

    Create Demo.sql

---
+++ Current Schema/Demo.sql
@@ -0,0 +1,12 @@
+INSERT INTO seller(visitor_id,ticket_id) VALUES
+(1,2122),
+(2,2123),
+(3,2124),
+(4,2125);
+
+
+SELECT * FROM resalequeue;
+
+INSERT INTO buyer(visitor_id, music_event_id, ticketType_type) VALUES
+(21, 35, 'REGULAR'),
+(22, 35, 'BACKSTAGE');
