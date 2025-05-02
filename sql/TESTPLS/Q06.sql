269e78420f07e52f33facce4b689025274d5d787
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-02 20:09:53 +0300

    Update Q06.sql
    τρέχει με μεταβλητό visitor_id, δεν τυπώνει πια κάθε φορά και το id και το όνομα του επισκέπτη, καθώς απλά επαναλαμβανόταν αυτό

--- sql/TESTPLS/Q06.sql
+++ sql/TESTPLS/Q06.sql
@@ -1,4 +1,4 @@
-SELECT visitor_id AS 'ID', visitor_name AS 'Name', music_event_id, AVG((review_interpretation + review_sound_and_lighting + review_stage_presence + review_organization + review_overall_impression)/5) AS 'Average Review'
-FROM visitor NATURAL JOIN review NATURAL JOIN performance NATURAL JOIN music_event
-GROUP BY visitor_id, visitor_name, music_event_id
-HAVING visitor_id = 5;
+SET @visitor_id = 66;
+WITH rev_event AS (SELECT music_event_id, (review_interpretation + review_sound_and_lighting + review_stage_presence + review_organization + review_overall_impression) AS score FROM review NATURAL JOIN performance NATURAL JOIN music_Event WHERE visitor_id = @visitor_id)
+SELECT  music_event_id, AVG(score) AS 'Average Review' FROM rev_event
+GROUP BY music_event_id;
