c63ab9739c05450fbd4c95c9055c740132708760
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-02 20:02:56 +0300

    Update Q04.sql
    τρέχει με μεταβλητή για artist_id + έβαλα υποστήριξη για αν για τον/την καλλιτέχνη δεν υπάρχει κανένα review

--- sql/TESTPLS/Q04.sql
+++ sql/TESTPLS/Q04.sql
@@ -1,7 +1,10 @@
-WITH band_rev (band_id, interpretation, overall_impression) AS (SELECT band_id, review_interpretation, review_overall_impression FROM band INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON band.band_performer_id = performer_id),
-A (artist_id, artist_name, interpretation, overall_impression) AS (SELECT artist_id, artist_name, review_interpretation, review_overall_impression FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN review ON artist.artist_performer_id = performer.performer_id),
-B (artist_id, artist_name, interpretation, overall_impression) AS (SELECT artist_id, artist_name, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev),
+SET @artist_id = 1;
+WITH band_rev AS (SELECT band_id, review_interpretation AS interpretation, review_overall_impression AS overall_impression FROM band INNER JOIN performer ON band.band_performer_id = performer_id NATURAL JOIN performance NATURAL JOIN review),
+A (artist_id, interpretation, overall_impression) AS (SELECT artist_id, review_interpretation, review_overall_impression FROM artist INNER JOIN performer ON artist.artist_performer_id = performer.performer_id NATURAL JOIN performance NATURAL JOIN review WHERE artist_id = @artist_id),
+B (artist_id, interpretation, overall_impression) AS (SELECT artist_id, interpretation, overall_impression FROM artist NATURAL JOIN members NATURAL JOIN band_rev WHERE artist_id = @artist_id),
 art_rev AS (SELECT * FROM A UNION SELECT * FROM B)
-SELECT artist_name AS 'Name', AVG(interpretation) AS 'Average Interpretation', AVG(overall_impression) AS 'Average Overall Impression' FROM art_rev
-WHERE artist_id = 1
+SELECT artist_name AS 'Name', AVG(interpretation) AS 'Average Interpretation', AVG(overall_impression) AS 'Average Overall Impression'
+FROM artist LEFT JOIN art_rev
+ON artist.artist_id = art_rev.artist_id
+WHERE artist.artist_id = @artist_id
 GROUP BY artist_name;
