4a69b605399736ff7927fcfb7fd01765ec67b794
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-02 20:20:09 +0300

    Update Q06.sql
    μεταβλητό visitor_id + τυπώνει NULL αν ο επισκέπτης δεν έχει αφήσει κριτική

--- sql/TESTPLS/Q06.sql
+++ sql/TESTPLS/Q06.sql
@@ -1,4 +1,5 @@
-SET @visitor_id = 66;
-WITH rev_event AS (SELECT music_event_id, (review_interpretation + review_sound_and_lighting + review_stage_presence + review_organization + review_overall_impression) AS score FROM review NATURAL JOIN performance NATURAL JOIN music_Event WHERE visitor_id = @visitor_id)
-SELECT  music_event_id, AVG(score) AS 'Average Review' FROM rev_event
+SET @visitor_id = 12;
+WITH rev_event AS (SELECT visitor_id, music_event_id, (review_interpretation + review_sound_and_lighting + review_stage_presence + review_organization + review_overall_impression) AS score FROM review NATURAL JOIN performance NATURAL JOIN music_Event WHERE visitor_id = @visitor_id)
+SELECT music_event_id, AVG(score) AS 'Average Review' FROM visitor LEFT JOIN rev_event ON visitor.visitor_id = rev_event.visitor_id
+WHERE visitor.visitor_id = @visitor_id
 GROUP BY music_event_id;
