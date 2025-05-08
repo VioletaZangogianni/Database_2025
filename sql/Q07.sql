873e5dd9bd7fb0fe59be257eb39456f28aba3f73
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-08 11:38:40 +0300

    Update Q07.sql

--- sql/Q07.sql
+++ sql/Q07.sql
@@ -1,5 +1,5 @@
-WITH fest_staff AS (SELECT festival_fest_year AS fest_year, festival_name AS fest_name, staff_id, staff_levels_id AS experience_level FROM festival NATURAL JOIN music_event NATURAL JOIN worksIn NATURAL JOIN staff NATURAL JOIN staff_levels GROUP BY festival_fest_year, festival_name, staff_id HAVING COUNT(*) > 0)
-SELECT fest_year, fest_name AS 'Year', AVG(experience_level) AS average_exp_level FROM fest_staff
+WITH fest_staff AS (SELECT festival_fest_year AS fest_year, festival_name AS fest_name, staff_id, staff_levels_id AS experience_level FROM festival NATURAL JOIN music_event NATURAL JOIN worksIn NATURAL JOIN staff NATURAL JOIN staff_levels GROUP BY fest_year, fest_name, staff_id HAVING COUNT(*) > 0)
+SELECT fest_year, fest_name AS 'Festival', AVG(experience_level) AS average_exp_level FROM fest_staff
 GROUP BY fest_year, fest_name
 ORDER BY average_exp_level ASC
 LIMIT 1;
