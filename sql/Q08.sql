e38e1c0eb91407d4389cab4a947edf7c80ebf8f7
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-08 13:21:51 +0300

    Update Q08.sql
    έβαλα μεταβλητή για την ημερομηνία + της έχω δώσει για τώρα τη ΜΟΝΗ τιμή στην οποία υπάρχει και προσωπικό που να δουλεύει, και προσωπικό που να μη δουλεύει

--- sql/Q08.sql
+++ sql/Q08.sql
@@ -1,3 +1,4 @@
+SET @event_date = '2028-09-14';
 SELECT DISTINCT staff_id AS ID, staff_name AS Name FROM staff
-WHERE staff_id NOT IN (SELECT staff_id FROM staff NATURAL JOIN worksIn NATURAL JOIN performance NATURAL JOIN music_event
-WHERE music_event_date = '2021-11-02');
+WHERE staff_id NOT IN (SELECT staff_id FROM staff NATURAL JOIN worksIn NATURAL JOIN music_event
+WHERE music_event_date = @event_date);
