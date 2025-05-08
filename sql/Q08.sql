36445caebd31d0f8fe561a4eed42c16c552199da
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-08 22:05:16 +0300

    Update Q08.sql
    δεν είχα δει ότι θέλουμε μόνο προσωπικό που να είναι Support
    έβαλα κατάλληλο subquery ώστε να ασχολούμαστε μόνο με αυτούς. ώστε να μειώσουμε και το φόρτο του DBSM

--- sql/Q08.sql
+++ sql/Q08.sql
@@ -1,4 +1,5 @@
 SET @event_date = '2028-09-14';
-SELECT DISTINCT staff_id AS ID, staff_name AS Name FROM staff
-WHERE staff_id NOT IN (SELECT staff_id FROM staff NATURAL JOIN worksIn NATURAL JOIN music_event
+WITH support AS (SELECT staff_id, staff_name FROM staff WHERE role_staff_desc = 'Support')
+SELECT staff_id AS ID, staff_name AS 'Name' FROM support
+WHERE staff_id NOT IN (SELECT staff_id FROM support NATURAL JOIN worksIn NATURAL JOIN music_event
 WHERE music_event_date = @event_date);
