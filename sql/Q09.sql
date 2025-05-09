b0b64cefc910699e4827665876610ebce1360829
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-09 10:17:58 +0300

    Update Q09.sql
    μετακίνησα ένα condition σε άλλο subquery για διευκόλυνση του DBMS

--- sql/Q09.sql
+++ sql/Q09.sql
@@ -1,5 +1,5 @@
 SET @start_date := '2019-09-12';
-WITH vis_event (visitor_id, visitor_name, shows) AS (SELECT visitor_id, CONCAT(visitor_name, ' ', visitor_surname) AS full_name, COUNT(DISTINCT music_event_id) FROM visitor NATURAL JOIN ticket NATURAL JOIN music_event WHERE music_event_date BETWEEN @start_date AND DATE_ADD(@start_date, INTERVAL 1 YEAR) GROUP BY visitor_id, full_name)
+WITH vis_event AS (SELECT visitor_id, CONCAT(visitor_name, ' ', visitor_surname) AS visitor_name, COUNT(DISTINCT music_event_id) AS shows FROM visitor NATURAL JOIN ticket NATURAL JOIN music_event WHERE music_event_date BETWEEN @start_date AND DATE_ADD(@start_date, INTERVAL 1 YEAR) GROUP BY visitor_id, visitor_name HAVING shows >= 3)
 SELECT V.shows AS Shows, V.visitor_id AS ID, V.visitor_name AS 'Name' FROM vis_event V
-WHERE V.shows IN (SELECT shows FROM vis_event WHERE visitor_id <> V.visitor_id AND shows >= 3)
+WHERE V.shows IN (SELECT shows FROM vis_event WHERE visitor_id <> V.visitor_id)
 ORDER BY V.shows ASC;
