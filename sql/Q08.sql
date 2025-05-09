3912eb638fbf72b32c6cb73b6466b9a1a3dcab75
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-09 09:47:44 +0300

    Update Q08.sql
    άλλαξα ημερομηνία
    λειτουργεί

--- sql/Q08.sql
+++ sql/Q08.sql
@@ -1,4 +1,4 @@
-SET @event_date = '2028-09-14';
+SET @event_date = '2020-05-14';
 WITH support AS (SELECT staff_id, staff_name FROM staff WHERE role_staff_desc = 'Support')
 SELECT staff_id AS ID, staff_name AS 'Name' FROM support
 WHERE staff_id NOT IN (SELECT staff_id FROM support NATURAL JOIN worksIn NATURAL JOIN music_event
