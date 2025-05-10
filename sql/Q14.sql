c76c2e82f222218683be1f70542faad4dc25c429
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 19:06:09 +0300

    Update Q14.sql

--- sql/Q14.sql
+++ sql/Q14.sql
@@ -3,7 +3,7 @@ A AS (SELECT genre_desc, performance_id, festival_fest_year AS fest_year FROM ar
 B AS (SELECT genre_desc, performance_id, fest_year FROM art2genre NATURAL JOIN artist NATURAL JOIN members NATURAL JOIN band_perf_year),
 C AS (SELECT genre_desc, performance_id, fest_year FROM band2genre NATURAL JOIN band_perf_year),
 g_perf_year AS (SELECT * FROM A UNION SELECT * FROM B UNION SELECT * FROM C),
-g_year_apps AS (SELECT genre_desc, fest_year, COUNT(*) AS appearances FROM g_perf_year GROUP BY genre_desc, fest_year)
-SELECT genre_desc AS 'Genre', fest_year AS 'First Year', fest_year+1 AS 'Second Year', appearances FROM g_year_apps WHERE (genre_desc, fest_year) IN (SELECT Y1.genre_desc, Y1.fest_year FROM g_year_apps Y1, g_year_apps Y2 WHERE Y1.fest_year + 1 = Y2.fest_year AND Y1.appearances = Y2.appearances)
-GROUP BY genre_desc, fest_year
-HAVING appearances >= 3;
+g_year_apps AS (SELECT genre_desc, fest_year, COUNT(*) AS appearances FROM g_perf_year GROUP BY genre_desc, fest_year HAVING appearances >= 3)
+SELECT G1.genre_desc AS Genre, G1.fest_year AS 'First Year', G2.fest_year AS 'Second Year', G1.appearances FROM g_year_apps G1, g_year_apps G2
+	WHERE G1.genre_desc = G2.genre_desc AND G1.fest_year + 1 = G2.fest_year AND G1.appearances = G2.appearances 
+	ORDER BY G1.genre_desc ASC, G1.fest_year ASC;
