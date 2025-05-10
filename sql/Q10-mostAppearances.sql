41263460fa6071d681b624473e1a84af7b53a6b6
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 11:03:44 +0300

    Update Q10-mostAppearances.sql
    καθάρισα λίγο τα subqueries και σκίπαρα τον performer στα JOINs

--- sql/Q10-mostAppearances.sql
+++ sql/Q10-mostAppearances.sql
@@ -1,8 +1,6 @@
-WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
-A (genre_desc, artist_id, performance_id) AS (SELECT genre_desc, artist_id, performance_id FROM art2genre NATURAL JOIN artist INNER JOIN performer NATURAL JOIN performance ON artist.artist_performer_id = performer.performer_id),
-B (genre_desc, artist_id, performance_id) AS (SELECT genre_desc, artist_id, performance_id FROM art2genre NATURAL JOIN artist NATURAL JOIN members NATURAL JOIN band_perf),
-g_art_perf (genre_desc, artist_id, performance_id) AS (SELECT * FROM A UNION SELECT * FROM B)
+WITH band_perf AS (SELECT band_id, performance_id FROM band INNER JOIN performance ON band.band_performer_id = performance.performer_id),
+A AS (SELECT genre_desc, artist_id, performance_id FROM art2genre NATURAL JOIN artist INNER JOIN performance ON artist.artist_performer_id = performance.performer_id),
+B AS (SELECT genre_desc, artist_id, performance_id FROM art2genre NATURAL JOIN artist NATURAL JOIN members NATURAL JOIN band_perf),
+g_art_perf AS (SELECT * FROM A UNION SELECT * FROM B)
 SELECT G1.genre_desc AS first_genre, G2.genre_desc AS second_genre, COUNT(G1.performance_id) AS apps FROM g_art_perf G1, g_art_perf G2 WHERE G1.artist_id = G2.artist_id AND G1.genre_desc < G2.genre_desc AND G1.performance_id = G2.performance_id
-GROUP BY first_genre, second_genre
-ORDER BY apps DESC
-LIMIT 3;
+GROUP BY first_genre, second_genre ORDER BY apps DESC LIMIT 3;
