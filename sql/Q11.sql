4ec6c6bc2668b086f4c7f40a8c7485e5507cdd5b
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 13:02:35 +0300

    Update Q11.sql
    μετακίνηση του COUNT σε subquery, ώστε να κάνει λιγότερες πράξεις το DBMS

--- sql/Q11.sql
+++ sql/Q11.sql
@@ -2,7 +2,7 @@ WITH band_perf AS (SELECT band_id, performance_id FROM band INNER JOIN performan
 	A AS (SELECT artist_id, artist_name, performance_id FROM artist INNER JOIN performance ON artist.artist_performer_id = performance.performer_id),
 	B AS (SELECT artist_id, artist_name, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
 	all_apps AS (SELECT * FROM A UNION SELECT * FROM B),
-	top_apps AS (SELECT artist_id, artist_name, COUNT(*)-4 as apps FROM all_apps GROUP BY artist_id, artist_name ORDER BY apps DESC LIMIT 1)
-SELECT artist_id, artist_name AS 'Name', COUNT(*) AS Appearances FROM all_apps
-	GROUP BY artist_id, artist_name
-	HAVING Appearances < ANY (SELECT apps FROM top_apps);
+    app_counts AS (SELECT artist_id, artist_name, COUNT(*) AS apps FROM all_apps GROUP BY artist_id, artist_name),
+	top_apps AS (SELECT apps-4 as most_apps FROM app_counts ORDER BY apps DESC LIMIT 1)
+SELECT artist_id, artist_name AS 'Name', apps AS Appearances FROM app_counts
+	WHERE apps < ANY (SELECT * FROM top_apps);
