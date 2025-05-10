6688cb08e8be94339d344632c5c48fed1680b323
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 13:33:43 +0300

    Update Q12.sql
    είχα γράψει τεχνικούς αντί για υποστηρικτικό

--- sql/Q12.sql
+++ sql/Q12.sql
@@ -1,5 +1,5 @@
 SET @f_id := 2;
 WITH day_ticket AS (SELECT music_event_date AS event_date, COUNT(ticket_id) AS audience FROM music_event NATURAL JOIN ticket WHERE festival_id = @f_id GROUP BY music_event_date),
-day_needs AS (SELECT event_date, CEILING(audience/20) AS security_needed, CEILING(audience/50) AS technicians_needed FROM day_ticket)
-SELECT event_date AS 'Day', security_needed + technicians_needed AS 'Staff Needed', security_needed AS 'Security Needed', technicians_needed AS 'Technicians Needed'
+day_needs AS (SELECT event_date, CEILING(audience/20) AS security_needed, CEILING(audience/50) AS support_needed FROM day_ticket)
+SELECT event_date AS 'Day', security_needed + support_needed AS 'Staff Needed', security_needed AS 'Security Needed', support_needed AS 'Support Needed'
 	FROM day_needs ORDER BY event_date ASC;
