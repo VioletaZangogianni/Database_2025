306e02860a28836e911a3ead2fc92447e2441703
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 20:32:52 +0300

    Update Q15.sql
    καθάρισα λίγο τα subqueries + έβαλα να τυπώνει τα ονόματα του επισκέπτη και του καλλιτέχνη

--- sql/Q15.sql
+++ sql/Q15.sql
@@ -1,7 +1,6 @@
-WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
-A (artist_id, performance_id) AS (SELECT artist_id, performance_id FROM artist INNER JOIN performer NATURAL JOIN performance ON artist.artist_performer_id = performer.performer_id),
-B (artist_id, performance_id) AS (SELECT artist_id, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
-art_perf (artist_id, performance_id) AS (SELECT * FROM A UNION SELECT * FROM B)
-SELECT visitor_id, artist_id, review_interpretation + review_overall_impression AS score FROM visitor NATURAL JOIN review NATURAL JOIN art_perf
-ORDER BY score DESC
-LIMIT 5;
+WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM band INNER JOIN performance ON band.band_performer_id = performance.performer_id),
+	A AS (SELECT artist_id, artist_name, performance_id FROM artist INNER JOIN performance ON artist.artist_performer_id = performance.performer_id),
+	B AS (SELECT artist_id, artist_name, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
+	art_perf AS (SELECT * FROM A UNION SELECT * FROM B)
+SELECT CONCAT(visitor_name, ' ', visitor_surname) AS Reviewer, artist_name AS Artist, review_interpretation + review_overall_impression AS Score FROM visitor NATURAL JOIN review NATURAL JOIN art_perf
+	ORDER BY Score DESC LIMIT 5;
