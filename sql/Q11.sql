4cc45dfbd512cb00a77fc42fc7af31b9af732280
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 11:26:16 +0300

    Update Q11.sql
    καθάρισα λίγο τα subqueries + σκιπάρω το performer στα JOINs

--- sql/Q11.sql
+++ sql/Q11.sql
@@ -1,10 +1,8 @@
-WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
-	 A (artist_id, artist_name, performance_id) AS (SELECT artist_id, artist_name, performance_id FROM artist INNER JOIN performer NATURAL JOIN performance ON artist.artist_performer_id = performer.performer_id),
-	 B (artist_id, artist_name, performance_id) AS (SELECT artist_id, artist_name, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
-	 top_apps (artist, artist_name, apps) AS (SELECT artist_id, artist_name, COUNT(*)-4 as apps FROM (SELECT * FROM A UNION SELECT * FROM B) AS BRUH
-		GROUP BY artist_id, artist_name
-		ORDER BY apps DESC
-		LIMIT 1)
-SELECT artist_id, artist_name AS 'Name', COUNT(*) AS Appearances FROM (SELECT * FROM A UNION SELECT * FROM B) AS BRUH
-    GROUP BY artist_id, artist_name
-    HAVING Appearances < ANY (SELECT apps FROM top_apps);
+WITH band_perf AS (SELECT band_id, performance_id FROM band INNER JOIN performance ON band.band_performer_id = performance.performer_id),
+	A AS (SELECT artist_id, artist_name, performance_id FROM artist INNER JOIN performance ON artist.artist_performer_id = performance.performer_id),
+	B AS (SELECT artist_id, artist_name, performance_id FROM artist NATURAL JOIN members NATURAL JOIN band_perf),
+	all_apps AS (SELECT * FROM A UNION SELECT * FROM B),
+	top_apps AS (SELECT artist_id, artist_name, COUNT(*)-4 as apps FROM all_apps GROUP BY artist_id, artist_name ORDER BY apps DESC LIMIT 1)
+SELECT artist_id, artist_name AS 'Name', COUNT(*) AS Appearances FROM all_apps
+	GROUP BY artist_id, artist_name
+	HAVING Appearances < ANY (SELECT apps FROM top_apps);
