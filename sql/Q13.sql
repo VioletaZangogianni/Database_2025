43e18b4c6f9be1132123ecc066f1c12cae639cb7
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-10 13:53:05 +0300

    Update Q13.sql
    καθάρισα λίγο τα subqueries + διόρθωσα λάθος επιλογή στο band_perf που προκαλούσε καρτεσιανό με το usic_event, αντί για NATURAL JOIN + έσβησα το DISTINCT, αφού το UNION δεν επιτρέπει duplicates

--- sql/Q13.sql
+++ sql/Q13.sql
@@ -1,7 +1,7 @@
-WITH band_perf (band_id, performance_id) AS (SELECT band_id, performance_id FROM band INNER JOIN performer NATURAL JOIN performance ON band.band_performer_id = performer.performer_id),
-	 A (artist_id, artist_name, continent) AS (SELECT artist_id, artist_name, location_continent FROM artist INNER JOIN performer NATURAL JOIN performance NATURAL JOIN music_event NATURAL JOIN festival NATURAL JOIN location ON artist.artist_performer_id = performer.performer_id),
-	 B (artist_id, artist_name, continent) AS (SELECT artist_id, artist_name, location_continent FROM artist NATURAL JOIN members NATURAL JOIN band_perf NATURAL JOIN music_event NATURAL JOIN festival NATURAL JOIN location),
+WITH band_perf AS (SELECT band_id, music_event_id FROM band INNER JOIN performance ON band.band_performer_id = performance.performer_id),
+	 A AS (SELECT artist_id, artist_name, location_continent AS continent FROM artist INNER JOIN performance NATURAL JOIN music_event NATURAL JOIN festival NATURAL JOIN location ON artist.artist_performer_id = performance.performer_id),
+	 B AS (SELECT artist_id, artist_name, location_continent AS continent FROM artist NATURAL JOIN members NATURAL JOIN band_perf NATURAL JOIN music_event NATURAL JOIN festival NATURAL JOIN location),
      art_cont (artist_id, artist_name, continent) AS (SELECT * FROM A UNION SELECT * FROM B)
-     SELECT artist_id AS ID, artist_name AS 'Name', COUNT(DISTINCT continent) AS different_continents FROM art_cont
-     GROUP BY artist_id, artist_name
-     HAVING different_continents > 3;
+SELECT artist_id AS ID, artist_name AS 'Name', COUNT(continent) AS different_continents FROM art_cont
+	GROUP BY artist_id, artist_name
+	HAVING different_continents > 3;
