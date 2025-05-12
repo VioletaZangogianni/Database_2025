8e4cfef12030fb9cf131db4e9ea3b614a9ef7860
STAMATIS MARIOS CHANTZOPOULOS <el22858@mail.ntua.gr>
2025-05-12 18:55:42 +0300

    Update Q01.sql

--- sql/Q01.sql
+++ sql/Q01.sql
@@ -2,7 +2,7 @@ WITH fest_ticket AS (SELECT festival_fest_year AS fest_year, festival_name AS fe
 			ticket_price AS price, ticketType_type AS ticket_Type, ticket_payment_method AS method
 		FROM festival NATURAL JOIN music_event LEFT JOIN ticket
         ON music_event.music_event_id = ticket.music_event_id),
-	fest_ticket_price AS (SELECT fest_year, fest_name,
+	fest_ticket_price AS (SELECT fest_year, fest_name, price,
 			CASE WHEN ticket_type = 'VIP' THEN price ELSE 0 END AS vip_price,
             CASE WHEN ticket_type = 'BACKSTAGE' THEN price ELSE 0 END AS back_price,
             CASE WHEN ticket_type = 'REGULAR' THEN price ELSE 0 END AS std_price,
@@ -11,21 +11,18 @@ WITH fest_ticket AS (SELECT festival_fest_year AS fest_year, festival_name AS fe
 			CASE WHEN method = 'DEBIT' THEN price ELSE 0 END AS debit_price,
             CASE WHEN method = 'CREDIT' THEN price ELSE 0 END AS credit_price,
             CASE WHEN method = 'BANK_DEPOSIT' THEN price ELSE 0 END AS bank_price
-		FROM fest_ticket),
-	fest_totals AS (SELECT fest_year, fest_name,
-		ROUND(SUM(vip_price), 2) AS vip_total, ROUND(SUM(back_price), 2) AS back_total,
-        ROUND(SUM(std_price), 2) AS std_total, ROUND(SUM(student_price), 2) AS student_total,
-        
-        ROUND(SUM(debit_price), 2) AS debit_total, ROUND(SUM(credit_price), 2) AS credit_total,
-		ROUND(SUM(bank_price), 2) AS bank_total
-    FROM fest_ticket_price
-    GROUP BY fest_year, fest_name)
+		FROM fest_ticket)
 SELECT fest_year AS 'Year', fest_name AS 'Festival',
-	CONCAT(ROUND(vip_total + back_total + std_total + student_total, 2), '€') AS 'Total Earnings',
-    CONCAT(std_total, '€') AS 'Regular ticket Earnings', CONCAT(vip_total, '€') AS 'VIP Earnings',
-    CONCAT(back_total, '€') AS 'Backstage Earnings', CONCAT(student_total, '€') AS 'Student ticket Earnings',
+	CONCAT(ROUND(SUM(price), 2), '€') AS 'Total Earnings',
     
-    CONCAT(debit_total, '€') AS 'Debit Earnings', CONCAT(credit_total, '€') AS 'Credit Earnings',
-    CONCAT(bank_total, '€') AS 'Bank Deposit Earnings'
-FROM fest_totals
+    CONCAT(ROUND(SUM(std_price), 2), '€') AS 'Regular ticket Earnings',
+    CONCAT(ROUND(SUM(vip_price), 2), '€') AS 'VIP Earnings',
+    CONCAT(ROUND(SUM(back_price), 2), '€') AS 'Backstage Earnings',
+    CONCAT(ROUND(SUM(student_price), 2), '€') AS 'Student ticket Earnings',
+    
+    CONCAT(ROUND(SUM(debit_price), 2), '€') AS 'Debit Earnings',
+    CONCAT(ROUND(SUM(credit_price), 2), '€') AS 'Credit Earnings',
+    CONCAT(ROUND(SUM(bank_price), 2), '€') AS 'Bank Deposit Earnings'
+FROM fest_ticket_price
+GROUP BY fest_year, fest_name
 ORDER BY fest_year ASC;
