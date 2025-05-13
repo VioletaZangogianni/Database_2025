438b6a45234c988fcfaf7b5f5954e125b74e5524
VioletaZangogianni <el22213@mail.ntua.gr>
2025-05-13 11:31:22 +0300

    Delete SQL directory

--- SQL/Q03-needsBands.sql
+++
@@ -1,4 +0,0 @@
-SELECT festival_id, artist_id FROM (SELECT * FROM artist natural join performer NATURAL JOIN performance NATURAL JOIN music_event
-WHERE performance_type="Warm-up") AS BRUUUUUUUH
-GROUP BY festival_id, artist_id
-HAVING COUNT(artist_id) > 2;
